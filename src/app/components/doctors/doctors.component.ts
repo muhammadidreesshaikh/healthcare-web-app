46d81587edd41c621834cb1cb46246d4ba4996aa
Muhammad Idrees Shaikh <idreesiqbal104@gmail.com>
2024-10-13 14:29:48 +0500

    doctor page design completed

--- src/app/components/doctors/doctors.component.ts
+++ src/app/components/doctors/doctors.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-doctors',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './doctors.component.html',
   styleUrl: './doctors.component.scss'
 })
